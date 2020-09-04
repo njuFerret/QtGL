3272cf0916b439f425920f50c8fbab98015f2935
njuFerret <emagazi.cn@gmail.com>
2020-09-04 10:04:15 +0800

    improved,

--- OGL02/openglwidget.cpp
+++ OGL02/openglwidget.cpp
@@ -23,7 +23,7 @@ void OpenGLWidget::initializeGL() {
   program->link();
   program->bind();
 
-  glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
+  glClearColor(0.0f, 0.3f, 0.0f, 1.0f);
   glEnable(GL_DEPTH_TEST);
 }
 
@@ -32,22 +32,37 @@ void OpenGLWidget::resizeGL(int w, int h) {}
 void OpenGLWidget::paintGL() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   // vertices array
-  GLfloat positionData[] = {-0.8f, -0.8f, 0.0f, 0.8f, -0.8f, 0.0f, 0.0f, 0.8f, 0.0f};
-  GLfloat colorData[] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+  QVector<GLfloat> positionData = {
+      -0.8f, -0.8f, 0.0f, // Point 1
+      0.8f,  -0.8f, 0.0f, // Point 2
+      0.0f,  0.8f,  0.0f  // Point 3
+  };
+  QVector<GLfloat> colorData = {
+      1.0f, 0.0f, 0.0f, // R: Point 1 color
+      0.0f, 1.0f, 0.0f, // G: Point 2 color
+      0.0f, 0.0f, 1.0f  // B: Point 3 color
+  };
+  //  QVector<GLuint> indices = {
+  //      0, 1, 3, //
+  //      1, 2, 3  //
+  //  };
 
   // vertex buffer object
   vbo.create();
   vbo.bind();
+  int size = positionData.size() + colorData.size();
+
   // vertex positions
-  vbo.allocate(positionData, 18 * sizeof(GLfloat));
+  vbo.allocate(positionData.data(), size * sizeof(GLfloat));
   GLuint vPosition = program->attributeLocation("VertexPosition");
   program->setAttributeBuffer(vPosition, GL_FLOAT, 0, 3, 0);
   glEnableVertexAttribArray(vPosition);
 
   // vertex colors
-  vbo.write(9 * sizeof(GLfloat), colorData, 9 * sizeof(GLfloat));
+  int offset = positionData.size();
+  vbo.write(offset * sizeof(GLfloat), colorData.data(), colorData.size() * sizeof(GLfloat));
   GLuint vColor = program->attributeLocation("VertexColor");
-  program->setAttributeBuffer(vColor, GL_FLOAT, 9 * sizeof(GLfloat), 3, 0);
+  program->setAttributeBuffer(vColor, GL_FLOAT, colorData.size() * sizeof(GLfloat), 3, 0);
   glEnableVertexAttribArray(vColor);
 
   // draw
