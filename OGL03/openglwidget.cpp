d33069e0fd06a236182ae625775a2af9fedd52d9
njuFerret <emagazi.cn@gmail.com>
2020-09-05 19:22:14 +0800

    稍微修改了一下

--- OGL03/openglwidget.cpp
+++ OGL03/openglwidget.cpp
@@ -29,7 +29,7 @@ void OpenGLWidget::initializeGL() {
   program->link();
   program->bind();
 
-  glClearColor(0.0f, 0.3f, 0.0f, 1.0f);
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glEnable(GL_DEPTH_TEST);
 }
 
@@ -37,21 +37,34 @@ void OpenGLWidget::resizeGL(int w, int h) {}
 
 void OpenGLWidget::paintGL() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  // vertices array
+  // 矩形顶点位置矩阵，需要6个顶点(1个面 x 每个面有2个三角形组成 x 每个三角形有3个顶点)
+  // 以下坐标以第一象限坐标为第0个顶点，按照逆时针依次第1、2、3个顶点
   QVector<GLfloat> positionData = {
-      -0.8f, -0.8f, 0.0f, // Point 1
-      0.8f,  -0.8f, 0.0f, // Point 2
-      0.0f,  0.8f,  0.0f  // Point 3
+      0.8f,  0.8f,  0.0f, // Point 0
+      0.8f,  -0.8f, 0.0f, // Point 1
+      -0.8f, 0.8f,  0.0f, // Point 3
+      0.8f,  -0.8f, 0.0f, // Point 1
+      -0.8f, -0.8f, 0.0f, // Point 2
+      -0.8f, 0.8f,  0.0f  // Point 3
   };
   QVector<GLfloat> colorData = {
-      1.0f, 0.0f, 0.0f, // R: Point 1 color
-      0.0f, 1.0f, 0.0f, // G: Point 2 color
-      0.0f, 0.0f, 1.0f  // B: Point 3 color
+      1.0f, 0.84f, 0.0f, // R: Point 1 color
+      1.0f, 0.84f, 0.0f, // R: Point 1 color
+      1.0f, 0.84f, 0.0f, // R: Point 1 color
+      1.0f, 0.84f, 0.0f, // R: Point 1 color
+      1.0f, 0.84f, 0.0f, // R: Point 1 color
+      1.0f, 0.84f, 0.0f  // R: Point 1 color
   };
-  //  QVector<GLuint> indices = {
-  //      0, 1, 3, //
-  //      1, 2, 3  //
-  //  };
+  //      1.0f, 0.0f, 0.0f, // R: Point 1 color
+  //      0.0f, 1.0f, 0.0f, // G: Point 2 color
+  //      0.0f, 0.0f, 1.0f, // B: Point 3 color
+  //      1.0f, 0.0f, 0.0f, // R: Point 1 color
+  //      0.0f, 1.0f, 0.0f, // G: Point 2 color
+  //      0.0f, 0.0f, 1.0f  // B: Point 3 color
+  QVector<GLuint> indices = {
+      0, 1, 3, //
+      1, 2, 3  //
+  };
 
   // vertex buffer object
   vbo.create();
@@ -80,18 +93,24 @@ void OpenGLWidget::paintGL() {
   //（如果不拿远几个单位，是看不到物体的，因为物体是和你的眼睛处在同一个平面了）；
   // rotate物体的旋转角度，第一个参数是旋转角度，后3个参数指定旋转轴。这里指定的就是z轴正方向。
 
-  QMatrix4x4 matrix;
-  matrix.perspective(90.0f, (GLfloat)w / (GLfloat)h, 0.001f, 100.0f);
-  matrix.translate(0.0, 0.0, -3);
-  matrix.rotate(angle, 0.0f, 0.0f, 1.0f);
+  QMatrix4x4 matrixModel;
+  matrixModel.rotate(angle, 0.0f, 0.0f, 1.0f); // 局部坐标沿Z洲选择angle角度
 
-  program->setUniformValue("RotationMatrix", matrix);
+  QMatrix4x4 matrixView;
+  matrixView.translate(0.0, 0.0, -3); // 视图矩阵沿-Z轴平移3个单位
 
-  // draw with mode Triangle
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  QMatrix4x4 matrixProjection;
+  matrixProjection.perspective(90.0f, (GLfloat)w / (GLfloat)h, 0.001f, 100.0f); // 投影矩阵沿垂直于平面(90°)
+
+  program->setUniformValue("model", matrixModel);
+  program->setUniformValue("view", matrixView);
+  program->setUniformValue("projection", matrixProjection);
+
+  // 绘制矩形，
+  glDrawArrays(GL_TRIANGLES, 0, indices.size());
 }
 
 void OpenGLWidget::onTimerOut() {
-  angle += 1.0;
+  angle += 1.0f;
   update();
 }
--- OGL03/shader/basic.vert
+++ OGL03/shader/basic.vert
@@ -12,12 +12,16 @@ in vec3 VertexColor;
 
 out vec3 Color;
 
-uniform mat4 RotationMatrix;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 
 void main()
 {
     // 用输入变量初始化输出变量的值
     Color = VertexColor;
     // 初始化一个预定义的vec4类型的变量gl_Position，设定该顶点的位置
-    gl_Position = RotationMatrix *vec4( VertexPosition, 1.0 );
+    gl_Position = projection * view * model *vec4( VertexPosition, 1.0 );
+
 }
